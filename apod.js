3919f9b2d27c3aad02fa1a5fa8c025f3e113fbb8
LariCostaSilva <larissa.costaesilva@hotmail.com>
2021-09-08 23:53:55 -0300

    Alterações de vídeo e imagem

--- apod.js
+++ apod.js
@@ -1,36 +1,41 @@
 //Referenciando tudo que está no html
 const principal = $(".corpo");
 const fundo = $("#imagem-apod");
+const video = $("#video-apod")
 const titulo = $(".titulo-imagem");
-const texto = $ ("#descricao");
-const botao = $ ("#botao");
+const texto = $("#descricao");
+const botao = $("#botao");
 
 botao.on('click', () => {
     apod()
 })
-function apod(){
+function apod() {
 
-$.ajax({
-   url: `https://api.nasa.gov/planetary/apod?api_key=3oJxNtB9Kez0obSunOfeSpROAwbPU9pd6zCgYWIf&date=${data.value}`, 
-   success(result){
-       fundo.css("background-image", `url(${result.hdurl})`);
-       titulo[0].innerHTML = result.title;
-       texto[0].innerHTML = result.explanation;
-       console.log (result);
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=3oJxNtB9Kez0obSunOfeSpROAwbPU9pd6zCgYWIf&date=${data.value}`,
+        success(result) {
+            fundo.css("background-image", `url(${result.hdurl})`);
+            titulo[0].innerHTML = result.title;
+            texto[0].innerHTML = result.explanation;
+            console.log(result);
 
-       if(result.media_type == "image"){
-        $(".Video").addClass("Oculto");
-        $(".Imagem").removeClass("Oculto")
-        $(".Imagem").html(`<img id="Foto" class="ImagemRecebida" src="${result.url}">`);
-    }
-    else
-    {
-        $(".Imagem").addClass("Oculto");
-        $(".Video").removeClass("Oculto")
-        
-        $(".Video").html(`<iframe class="VideoNasa" id="Video" src="${result.url}" frameborder="0"></iframe>`)
-    }
-   }
-})
+            //condição para quando for imagem, adiciona uma classe e remove outra
+            if (result.media_type == "image"){
+                $("#video-apod").addClass("");
+                $("#imagem-apod").removeClass("")
+                $("#imagem.apod").html(`<img id="imagem-apod" src="${result.url}">`);
+            }
+            
+            //quando for vídeo trocar as condições
+            else {
+                $("#imagem-apod").addClass("");
+                $("#video-apod").removeClass("")
+                video.html(`<iframe id="video-apod" src="${result.url}" frameborder="0"></iframe>`)
+            }
+            //error: function(){
+            //alert("insira uma data válida")
+           //}
+        }
+    })
 }
 apod()
